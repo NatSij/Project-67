85549e23b09eb9d94117dc06494c57e56cd164b1
NatSij <82244643+NatSij@users.noreply.github.com>
2023-07-12 17:27:57 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyD4-UyVxdQ-tFvBBpkyQNhHszwCHub-hjM",
+  authDomain: "project-67-170a8.firebaseapp.com",
+  projectId: "project-67-170a8",
+  storageBucket: "project-67-170a8.appspot.com",
+  messagingSenderId: "656097641277",
+  appId: "1:656097641277:web:f6cbe1369bf549a6c6fb54"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
